c0285581e40135d365336e71d909a71e1b8177a4
Aleksei Kuznetcov <kuznetz.vrn@gmail.com>
2017-02-12 17:41:10 -0500

    Added Post Detail template, Implement diynamic URL Routing

--- src/posts/views.py
+++ src/posts/views.py
@@ -1,21 +1,27 @@
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+
+from .models import Post
 
 
 def post_create(request):
     return HttpResponse('create')
 
 
-def post_detail(request):
+def post_detail(request, id):
+    instance = get_object_or_404(Post, id=id)
     context = {
-        "title": "Detail"
+        "title": instance.title,
+        "instance": instance
     }
-    return render(request, 'index.html', context)
+    return render(request, 'post_detail.html', context)
 
 
 def post_list(request):
+    queryset = Post.objects.all()
     if request.user.is_authenticated():
         context = {
+            "object_list": queryset,
             "title": "My User List"
         }
     else:
