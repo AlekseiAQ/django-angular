93d424349e556fb0f51e2bcdc9d7427962bc3063
Aleksei Kuznetcov <kuznetz.vrn@gmail.com>
2017-02-12 23:27:11 -0500

    Added simple Social Share Links and Custom Template Tag

--- src/posts/views.py
+++ src/posts/views.py
@@ -1,3 +1,5 @@
+from urllib.parse import quote_plus
+
 from django.contrib import messages
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.http import HttpResponse, HttpResponseRedirect
@@ -22,9 +24,11 @@ def post_create(request):
 
 def post_detail(request, id=None):
     instance = get_object_or_404(Post, id=id)
+    share_string = quote_plus(instance.title)
     context = {
         "title": instance.title,
-        "instance": instance
+        "instance": instance,
+        "share_string": share_string
     }
     return render(request, 'post_detail.html', context)
 
