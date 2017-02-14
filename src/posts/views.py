caf65b3d333dabe6c684d9d39a363762fa6594a4
Aleksei Kuznetcov <kuznetz.vrn@gmail.com>
2017-02-13 22:46:35 -0500

    Added Basic User Permissions

--- src/posts/views.py
+++ src/posts/views.py
@@ -2,7 +2,7 @@ from urllib.parse import quote_plus
 
 from django.contrib import messages
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
-from django.http import HttpResponse, HttpResponseRedirect
+from django.http import HttpResponse, HttpResponseRedirect, Http404
 from django.shortcuts import render, get_object_or_404, redirect
 
 from .forms import PostForm
@@ -10,6 +10,8 @@ from .models import Post
 
 
 def post_create(request):
+    if not request.user.is_staff or not request.user.is_superuser:
+        raise Http404
     form = PostForm(request.POST or None, request.FILES or None)
     if form.is_valid():
         instance = form.save(commit=False)
@@ -56,6 +58,8 @@ def post_list(request):
 
 
 def post_update(request, id=None):
+    if not request.user.is_staff or request.user.is_superuser:
+        raise Http404
     instance = get_object_or_404(Post, id=id)
     form = PostForm(request.POST or None, request.FILES or None, instance=instance)
     if form.is_valid():
