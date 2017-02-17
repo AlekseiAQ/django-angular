48a0aec28cd5d60711f82eac4b14a11357a79861
Aleksei Kuznetcov <kuznetz.vrn@gmail.com>
2017-02-16 22:42:40 -0500

    Django Generic Foreign Keys, Part 2

--- src/comments/models.py
+++ src/comments/models.py
@@ -1,4 +1,7 @@
 from django.conf import settings
+from django.contrib.contenttypes.fields import GenericForeignKey
+from django.contrib.contenttypes.models import ContentType
+
 from django.db import models
 
 from posts.models import Post
@@ -6,7 +9,12 @@ from posts.models import Post
 
 class Comment(models.Model):
     user = models.ForeignKey(settings.AUTH_USER_MODEL, default=1)
-    post = models.ForeignKey(Post)
+    # post = models.ForeignKey(Post)
+
+    content_type = models.ForeignKey(ContentType, on_delete=models.CASCADE)
+    object_id = models.PositiveIntegerField()
+    content_object = GenericForeignKey('content_type', 'object_id')
+
     content = models.TextField()
     timestamp = models.DateTimeField(auto_now_add=True)
 
