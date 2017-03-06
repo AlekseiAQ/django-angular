f7bdb9bdd42aa838059f8c5e0b7507f1365e8f97
Aleksei Kuznetcov <kuznetz.vrn@gmail.com>
2017-03-05 19:47:06 -0500

    Implemented UserLogin API Validation

--- src/accounts/api/serializers.py
+++ src/accounts/api/serializers.py
@@ -1,6 +1,6 @@
 from django.contrib.contenttypes.models import ContentType
 from django.contrib.auth import get_user_model
-
+from django.db.models import Q
 
 from rest_framework.serializers import (
     CharField,
@@ -64,8 +64,8 @@ class UserCreateSerializer(ModelSerializer):
 
 class UserLoginSerializer(ModelSerializer):
     token = CharField(allow_blank=True, read_only=True)
-    username = CharField()
-    email = EmailField(label="Email Address")
+    username = CharField(required=False, allow_blank=True)
+    email = EmailField(label="Email Address", required=False, allow_blank=True)
 
     class Meta:
         model = User
@@ -77,9 +77,28 @@ class UserLoginSerializer(ModelSerializer):
         ]
         extra_kwargs = {"password": {"write_only": True}}
 
-    # def validate(self, data):
-    #     email = data['email']
-    #     user_qs = User.objects.filter(email=email2)
-    #     if user_qs.exists():
-    #         raise ValidationError("This email already used.")
-    #     return data
+    def validate(self, data):
+        user_obj = None
+        email = data.get("email", None)
+        username = data.get("username", None)
+        password = data["password"]
+        if not email or not username:
+            raise ValidationError("A username or email is required to login.")
+
+        user = User.objects.filter(
+            Q(email=email) |
+            Q(username=username)
+        ).distinct()
+        user = user.exclude(email__isnull=True).exclude(email__iexact='')
+        if user.exists() and user.count() == 1:
+            user_obj = user.first()
+        else:
+            raise ValidationError("This username/email is not valid.")
+
+        if user_obj:
+            if not user_obj.check_password(password):
+                raise ValidationError("Incorrect credentials please try again.")
+
+        data["token"] = "SOME RANDOM TOKEN"
+
+        return data
