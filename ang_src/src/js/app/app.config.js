7281cbd9a80b19edd83553c66738eb9f2288a3d6
Aleksei Kuznetcov <kuznetz.vrn@gmail.com>
2017-03-07 22:52:56 -0500

    Initial Commit for the Combined Project

---
+++ ang_src/src/js/app/app.config.js
@@ -0,0 +1,29 @@
+'use strict';
+
+angular.module('try').
+    config(
+        function(
+            $locationProvider,
+            $routeProvider
+            ){
+            $locationProvider.html5Mode({
+                enabled:true
+            })
+            $routeProvider.
+                when("/", {
+                    template: "<blog-list></blog-list>"
+                }).
+                when("/about", {
+                    templateUrl: "/templates/about.html"
+                }).
+                when("/blog", {
+                    template: "<blog-list></blog-list>"
+                    // redirectTo: "/"
+                }).
+                when("/blog/:id", {
+                    template: "<blog-detail></blog-detail>"
+                }).
+                otherwise({
+                    template: "Not Found"
+                })
+    });
