091d7698f4b8f6ae4b9ac00cd895a81c73ae5ca1
Aleksei Kuznetcov <kuznetz.vrn@gmail.com>
2017-03-12 00:36:49 -0500

    Create Comment in Backend with http

--- src/static/js/app/blog-detail/blog-detail.component.js
+++ src/static/js/app/blog-detail/blog-detail.component.js
@@ -3,9 +3,11 @@
 angular.module('blogDetail').
     component('blogDetail', {
         templateUrl: '/api/templates/blog-detail.html',
-        controller: function(Post, $http, $location, $routeParams, $scope){
-            Post.get({"slug": $routeParams.slug}, function(data) {
+        controller: function(Post, $cookies, $http, $location, $routeParams, $scope){
+            var slug = $routeParams.slug
+            Post.get({"slug": slug}, function(data) {
                 $scope.post = data
+                $scope.comments = data.comments
             })
             // Post.query(function(data){
             //     $scope.notFound = true
@@ -33,15 +35,35 @@ angular.module('blogDetail').
 
             $scope.addReply = function() {
                 console.log($scope.reply)
-                $scope.comments.push($scope.reply)
-                // $scope.comments.push("abc")
-                resetReply()
+                var token = $cookies.get("token")
+                if (token) {
+                    var req = {
+                        method: "POST",
+                        url: '' + slug + '',
+                        data: {
+                            content: $scope.reply.content
+                        },
+                        headers: {
+                            authorization: "JWT " + token
+                        }
+                    }
+                    var requestAction = $http(req)
+                    requestAction.then(function(response) {
+                        $scope.comments.push($scope.reply)
+                        resetReply()
+                    }, function(e_response) {
+                        console.log("Error. Statuts code: " + e_response.status)
+                    })
+                    // $scope.comments.push("abc")
+                } else {
+                    console.log("no token")
+                }
             }
 
             function resetReply(){
                 $scope.reply = {
-                    "id": $scope.comments.length + 1,
-                    "text": "",
+                    id: $scope.comments.length + 1,
+                    content: "",
                 }
             }
 
